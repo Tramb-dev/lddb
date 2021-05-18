66af144f909834e059e9ac011ed638e232be43d9
Tramb-dev <83473808+Tramb-dev@users.noreply.github.com>
2021-05-18 11:51:44 +0200

    Create vainqueur.php

---
+++ tutos/vainqueur.php
@@ -0,0 +1,1159 @@
+<?php 
+	include 'header.php';
+?>
+
+<table align="center">
+	<caption>Nombre de zones pour les titres de vainqueur</caption>
+	
+	<tr>
+		<th>Titre</th>
+		<th>Campagne</th>
+		<th>Nombre de zones</th>
+	</tr>
+	<tr>
+		<td>Vainqueur de Tyrie</td>
+		<td>Prophéties</td>
+		<td>54</td>
+	</tr>
+	<tr>
+		<td>Vainqueur Canthien(ne)</td>
+		<td>Factions</td>
+		<td>33</td>
+	</tr>
+	<tr>
+		<td>Vainqueur Elonien(ne)</td>
+		<td>Nightfall</td>
+		<td>34</td>
+	</tr>
+	<tr>
+		<td>Maître du Nord </td>
+		<td>Gwen</td>
+		<td>15</td>
+</table>
+
+	<h3>Index :</h3> 
+
+	<strong><em>Tyrie :</em></strong>
+	<ul>
+		<li><a href="#Ascalon">Ascalon</a></li>
+		<li><a href="#CimesfroidesN">Cimefroides du Nord</a></li>
+		<li><a href="#Kryte">La Kryte</a></li>
+		<li><a href="#Maguuma">La Jungle Maguuma</a></li>
+		<li><a href="#Desert">Le Désert de Cristal</a></li>
+		<li><a href="#CimesfroidesS">Cimefroides du Sud</a></li>
+		<li><a href="#Archipel">L'Archipel des Iles de Feu</a></li>
+	</ul>
+	
+	<strong><em>Cantha :</em></strong>
+	<ul>
+		<li><a href="#Ile">Ile de Shing Jea</a></li>
+		<li><a href="#Cite">Cité de Kaineng</a></li>
+		<li><a href="#Foret">Forêt d'Echovald</a></li>
+		<li><a href="#Mer">Mer de Jade</a></li>
+	</ul>
+
+	<strong><em>Elona :</em></strong>
+	<ul>
+		<li><a href="#Istan">Istan</a></li>
+		<li><a href="#Kourna">Kourna</a></li>
+		<li><a href="#Vabbi">Vabbi</a></li>
+		<li><a href="#Desolation">La Désolation</a></li>
+		<li><a href="#Tourment">Le Royaume du Tourment</a></li>
+	</ul>
+	
+	<strong><em>Gwen :</em></strong>
+	<ul>
+		<li><a href="#Profondeur">Profondeur des Cimesfroides</a></li>
+		<li><a href="#char">Char</a></li>
+		<li><a href="#cotes">Côtes ternies</a></li>
+	</ul>
+<br/>
+<br/>
+<br/>
+
+<!-- Nombre de moobs en Tyrie (Prophéties)  -->
+
+<table align="center">
+	<caption>Tyrie</caption>
+	
+	<tr>
+		<th>Zone d'exploration</th>
+		<th>Nombre moyen</th>
+		<th>Nombre mimimum</th>
+		<th>Nombre maximun</th>
+		<th>Po et xp moyen</th>
+	</tr>
+	<tr>
+		<td colspan="5"><span id="Ascalon"><strong>Ascalon</strong></span></td>
+	</tr>
+	<tr>
+		<td>Basses-terres de Diessa</td>
+		<td>360</td>
+		<td>333</td>
+		<td>387</td>
+		<td>1800</td>
+	</tr>
+	<tr>
+		<td>Collines d'Ascalon</td>
+		<td>55</td>
+		<td>51</td>
+		<td>59</td>
+		<td>275</td>
+	</tr>
+	<tr>
+		<td>Corridor du Temple de la Flamme</td>
+		<td>104</td>
+		<td>101</td>
+		<td>107</td>
+		<td>520</td>
+	</tr>
+	<tr>
+		<td>Frontière orientale</td>
+		<td>193</td>
+		<td>170</td>
+		<td>216</td>
+		<td>965</td>
+	</tr>
+	<tr>
+		<td>Gosier du Dragon</td>
+		<td>129</td>
+		<td>122</td>
+		<td>135</td>
+		<td>645</td>
+	</tr>
+	<tr>
+		<td>La Brèche du Rampart</td>
+		<td>123</td>
+		<td>111</td>
+		<td>134</td>
+		<td>615</td>
+	</tr>
+	<tr>
+		<td>Plaines Grêlées</td>
+		<td>193</td>
+		<td>155</td>
+		<td>230</td>
+		<td>965</td>
+	</tr>
+	<tr>
+		<td>Vallée du Régent</td>
+		<td>152</td>
+		<td>135</td>
+		<td>168</td>
+		<td>760</td>
+	</tr>
+	<tr>
+		<td>Vieil Ascalon</td>
+		<td>246</td>
+		<td>200</td>
+		<td>291</td>
+		<td>1230</td>
+	</tr>
+	<tr>
+		<td colspan="5"><span id="CimesfroidesN"><strong>Cimesfroides du Nord</strong></span></td>
+	</tr>
+	<tr>
+		<td>Bassin de Deldrimor</td>
+		<td>173</td>
+		<td>150</td>
+		<td>195</td>
+		<td>865</td>
+	</tr>
+	<tr>
+		<td>Gueule du Griffon</td>
+		<td>48</td>
+		<td>43</td>
+		<td>52</td>
+		<td>240</td>
+	</tr>
+	<tr>
+		<td>Mine du Cheval de fer</td>
+		<td>175</td>
+		<td>160</td>
+		<td>190</td>
+		<td>875</td>
+	</tr>
+	<tr>
+		<td>Rocher de l'Enclume</td>
+		<td>193</td>
+		<td>137</td>
+		<td>248</td>
+		<td>965</td>
+	</tr>
+	<tr>
+		<td>Val du pèlerin</td>
+		<td>178</td>
+		<td>164</td>
+		<td>191</td>
+		<td>890</td>
+	</tr>
+	<tr>
+		<td colspan="5"><span id="Kryte"><strong>La Kryte</strong></span></td>
+	</tr>
+	<tr>
+		<td>Côte de la Raie</td>
+		<td>160</td>
+		<td>152</td>
+		<td>168</td>
+		<td>800</td>
+	</tr>
+		<tr>
+		<td>Côte des Tours de Guet</td>
+		<td>161</td>
+		<td>152</td>
+		<td>169</td>
+		<td>805</td>
+	</tr>
+	<tr>
+		<td>Kryte du Nord</td>
+		<td>184</td>
+		<td>126</td>
+		<td>241</td>
+		<td>920</td>
+	</tr>	
+	<tr>
+		<td>Lacs des Deux Serpents</td>
+		<td>183</td>
+		<td>171</td>
+		<td>194</td>
+		<td>915</td>
+	</tr>	
+	<tr>
+		<td>Lande de Talmark</td>
+		<td>230</td>
+		<td>219</td>
+		<td>240</td>
+		<td>1150</td>
+	</tr> 
+	<tr>
+		<td>Larmes des Déchus</td>
+		<td>67</td>
+		<td>65</td>
+		<td>69</td>
+		<td>335</td>
+	</tr>
+	<tr>
+		<td>Le Rideau noir</td>
+		<td>233</td>
+		<td>228</td>
+		<td>237</td>
+		<td>1165</td>
+	</tr> 
+	<tr>
+		<td>Nebo Terrace</td>
+		<td>151</td>
+		<td>138</td>
+		<td>164</td>
+		<td>755</td>
+	</tr>
+	<tr>
+		<td>Pente des Brigands</td>
+		<td>82</td>
+		<td>68</td>
+		<td>95</td>
+		<td>410</td>
+	</tr>
+	<tr>
+		<td>Pic Kesse</td>
+		<td>173</td>
+		<td>156</td>
+		<td>189</td>
+		<td>865</td>
+	</tr>
+	<tr>
+		<td>Refuge royal</td>
+		<td>65</td>
+		<td>51</td>
+		<td>79</td>
+		<td>325</td>
+	</tr>
+	<tr>
+		<td>Terres Maudites</td>
+		<td>76</td>
+		<td>72</td>
+		<td>79</td>
+		<td>380</td>
+	</tr>
+	
+	<tr>
+		<td colspan="5"><span id="Maguuma"><strong>La Jungle Maguuma</strong></span></td>
+	</tr>
+	<tr>
+		<td>Bois-Argent</td>
+		<td>210</td>
+		<td>199</td>
+		<td>221</td>
+		<td>1050</td>
+	</tr>
+	<tr>
+		<td>Cimesèche</td>
+		<td>58</td>
+		<td>55</td>
+		<td>61</td>
+		<td>290</td>
+	</tr>
+	<tr>
+		<td>Echin d'Ettin</td>
+		<td>196</td>
+		<td>176</td>
+		<td>216</td>
+		<td>980</td>
+	</tr>
+	<tr>
+		<td>Lagon de Mamnoun</td>
+		<td>57</td>
+		<td>56</td>
+		<td>58</td>
+		<td>285</td>
+	</tr>
+	<tr>
+		<td>Les Chutes</td>
+		<td>303</td>
+		<td>284</td>
+		<td>322</td>
+		<td>1515</td>
+	</tr>
+	<tr>
+		<td>Marais du Roseau</td>
+		<td>85</td>
+		<td>67</td>
+		<td>102</td>
+		<td>425</td>
+	</tr>
+	<tr>
+		<td>Racine enchevêtrante</td>
+		<td>215</td>
+		<td>191</td>
+		<td>239</td>
+		<td>1075</td>
+	</tr>
+	<tr>
+		<td>Saugeterres</td>
+		<td>259</td>
+		<td>224</td>
+		<td>294</td>
+		<td>1295</td>
+	</tr>
+	
+	
+	<tr>
+		<td colspan="5"><span id="Desert"><strong>Le Désert de Cristal</strong></span></td>
+	</tr>
+	<tr>
+		<td>Chemin du Prophète</td>
+		<td>265</td>
+		<td>214</td>
+		<td>315</td>
+		<td>1325</td>
+	</tr>
+	<tr>
+		<td>Etendues célestes</td>
+		<td>170</td>
+		<td>162</td>
+		<td>178</td>
+		<td>850</td>
+	</tr>
+	<tr>
+		<td>La Cicatrice</td>
+		<td>260</td>
+		<td>248</td>
+		<td>271</td>
+		<td>1300</td>
+	</tr>
+	<tr>
+		<td>La Mer Aride</td>
+		<td>107</td>
+		<td>87</td>
+		<td>126</td>
+		<td>535</td>
+	</tr>
+	<tr>
+		<td>Montée du Devin</td>
+		<td>132</td>
+		<td>108</td>
+		<td>156</td>
+		<td>660</td>
+	</tr>
+	<tr>
+		<td>Moraines du Vautour</td>
+		<td>238</td>
+		<td>222</td>
+		<td>253</td>
+		<td>1190</td>
+	</tr>
+	<tr>
+		<td>Plaines Salines</td>
+		<td>196</td>
+		<td>157</td>
+		<td>234</td>
+		<td>980</td>
+	</tr>
+	
+	<tr>
+		<td colspan="5"><span id="CimesfroidesS"><strong>Cimesfroides du Sud</strong></span></td>
+	</tr>
+	<tr>
+		<td>Banquise de Glace</td>
+		<td>208</td>
+		<td>191</td>
+		<td>224</td>
+		<td>1040</td>
+	</tr>	
+	<tr>
+		<td>Congère cuirassée</td>
+		<td>246</td>
+		<td>112</td>
+		<td>380</td>
+		<td>1230</td>
+	</tr>
+	<tr>
+		<td>Danse du Serpent</td>
+		<td>360</td>
+		<td>314</td>
+		<td>406</td>
+		<td>1800</td>
+	</tr>  	 	 
+	<tr>
+		<td>Dome de glace</td>
+		<td>130</td>
+		<td>120</td>
+		<td>140</td>
+		<td>650</td>
+	</tr>
+	<tr>
+		<td>Forêt gelée</td>
+		<td>313</td>
+		<td>307</td>
+		<td>319</td>
+		<td>1565</td>
+	</tr>
+	<tr>
+		<td>L'Empreinte de Grenth</td>
+		<td>241</td>
+		<td>238</td>
+		<td>243</td>
+		<td>1205</td>
+	</tr>
+	<tr>
+		<td>Le palais de Witman</td>
+		<td>102</td>
+		<td>92</td>
+		<td>112</td>
+		<td>510</td>
+	</tr>  	 	 
+	<tr>
+		<td>Passage de Lornar</td>
+		<td>285</td>
+		<td>279</td>
+		<td>291</td>
+		<td>1425</td>
+	</tr>
+	<tr>
+		<td>Pic de la Tête de Lance</td>
+		<td>225</td>
+		<td>216</td>
+		<td>234</td>
+		<td>1125</td>
+	</tr>   
+	<tr>
+		<td>Pente de Talus</td>
+		<td>391</td>
+		<td>377</td>
+		<td>405</td>
+		<td>1955</td>
+	</tr>
+	<tr>
+		<td>Sources minérales</td>
+		<td>193</td>
+		<td>182</td>
+		<td>204</td>
+		<td>965</td>
+	</tr>
+	
+	<tr>
+		<td colspan="5"><span id="Archipel"><strong>L'Archipel des Iles de Feu</strong></span></td>
+	</tr>
+	<tr>
+		<td>Roc de la Perdition</td>
+		<td>229</td>
+		<td>218</td>
+		<td>240</td>
+		<td>1145</td>
+	</tr>
+</table>
+
+<br/>
+			
+<br/>
+<!-- Nombre de moobs dans cantha (faction)  -->
+<table align="center">
+	<caption>Cantha</caption>
+	
+	<tr>
+		<th>Zone d'exploration</th>
+		<th>Nombre moyen</th>
+		<th>Nombre mimimum</th>
+		<th>Nombre maximun</th>
+		<th>Po et xp moyen</th>
+	</tr>
+	<tr>
+		<td colspan="5"><span id="Ile"><strong>Ile de Shing Jea</strong></span></td>
+	</tr>
+	<tr>
+		<td>Chemin de Saoshang</td>
+		<td>31</td>
+		<td>24</td>
+		<td>37</td>
+		<td>155</td>
+	</tr>
+	<tr>
+		<td>Domaine du minstre Cho</td>
+		<td>213</td>
+		<td>207</td>
+		<td>218</td>
+		<td>1065</td>
+	</tr>
+	<tr>
+		<td>Lagon d'Haiju</td>
+		<td>171</td>
+		<td>158</td>
+		<td>183</td>
+		<td>855</td>
+	</tr>
+	<tr>
+		<td>Péninsule de Panjiang</td>
+		<td>188</td>
+		<td>175</td>
+		<td>200</td>
+		<td>940</td>
+	</tr>
+	<tr>
+		<td>Promontoire de Jaya</td>
+		<td>192</td>
+		<td>182</td>
+		<td>202</td>
+		<td>960</td>
+	</tr>
+	<tr>
+		<td>Province de Kinya</td>
+		<td>134</td>
+		<td>123</td>
+		<td>145</td>
+		<td>670</td>
+	</tr>
+	<tr>
+		<td>Valée de Sunqua</td>
+		<td>127</td>
+		<td>105</td>
+		<td>149</td>
+		<td>635</td>
+	</tr>
+	<tr>
+		<td>Zen Daijun</td>
+		<td>163</td>
+		<td>160</td>
+		<td>166</td>
+		<td>815</td>
+	</tr>
+
+	<tr>
+		<td colspan="5"><span id="Cite"><strong>Cité de Kaineng</strong></span></td>
+	</tr>
+	<tr>
+		<td>Bazar de Wajjun</td>
+		<td>168</td>
+		<td>90</td>
+		<td>245</td>
+		<td>840</td>
+	</tr>
+	<tr>
+		<td>Chemin de Bukdek</td>
+		<td>108</td>
+		<td>63</td>
+		<td>152</td>
+		<td>540</td>
+	</tr>
+	<tr>
+		<td>District de Sunjiang</td>
+		<td>203</td>
+		<td>196</td>
+		<td>210</td>
+		<td>1015</td>
+	</tr>
+	<tr>
+		<td>Palais de Raisu</td>
+		<td>252</td>
+		<td>244</td>
+		<td>260</td>
+		<td>1260</td>
+	</tr>
+	<tr>
+		<td>Passage de l'ombre</td>
+		<td>15</td>
+		<td>8</td>
+		<td>22</td>
+		<td>75</td>
+	</tr>
+	<tr>
+		<td>Quartiers de Nahpui</td>
+		<td>110</td>
+		<td>108</td>
+		<td>112</td>
+		<td>55O</td>
+	</tr>
+	<tr>
+		<td>Sentier de Xaquang</td>
+		<td>160</td>
+		<td>153</td>
+		<td>166</td>
+		<td>800</td>
+	</tr>
+	<tr>
+		<td>Temple de Tahnnakai</td>
+		<td>126</td>
+		<td>121</td>
+		<td>130</td>
+		<td>630</td>
+	</tr>
+	<tr>
+		<td>Tunnels de Shenzun</td>
+		<td>239</td>
+		<td>233</td>
+		<td>244</td>
+		<td>1195</td>
+	</tr>
+	<tr>
+		<td>Valée de Pongmei</td>
+		<td>151</td>
+		<td>140</td>
+		<td>162</td>
+		<td>755</td>
+	</tr>
+
+	<tr>
+		<td colspan="5"><span id="Foret"><strong>Forêt d'Echovald</strong></span></td>
+	</tr>
+	<tr>
+		<td>Brousses de Drazach</td>
+		<td>220</td>
+		<td>207</td>
+		<td>232</td>
+		<td>1100</td>
+	</tr>  
+	<tr>
+		<td>Chemin de Morostav</td>
+		<td>296</td>
+		<td>286</td>
+		<td>305</td>
+		<td>1480</td>
+	</tr>
+	<tr>
+		<td>Chutes du Deuil</td>
+		<td>218</td>
+		<td>214</td>
+		<td>222</td>
+		<td>1090</td>
+	</tr>
+	<tr>
+		<td>Espoir de Melandru</td>
+		<td>168</td>
+		<td>114</td>
+		<td>221</td>
+		<td>840</td>
+	</tr>	
+	<tr>
+		<td>Le Verger Eternel</td>
+		<td>237</td>
+		<td>226</td>
+		<td>247</td>
+		<td>1185</td>
+	</tr>
+	<tr>
+		<td>Pierre Arborea</td>
+		<td>330</td>
+		<td>306</td>
+		<td>353</td>
+		<td>1650</td>
+	</tr>
+	<tr>
+		<td>Vallon de la Fougère</td>
+		<td>277</td>
+		<td>233</td>
+		<td>320</td>
+		<td>1385</td>
+	</tr>
+	
+	<tr>
+			<td colspan="5"><span id="Mer"><strong>Mer de Jade</strong></span></td>
+	</tr>
+	<tr>
+		<td>Collines de Maishang</td>
+		<td>181</td>
+		<td>153</td>
+		<td>209</td>
+		<td>905</td>
+	</tr>
+	<tr>
+		<td>Couvoir de Gyala</td>
+		<td>196</td>
+		<td>183</td>
+		<td>209</td>
+		<td>980</td>
+	</tr>
+	<tr>
+		<td>Cratère de Rhea</td>
+		<td>251</td>
+		<td>230</td>
+		<td>271</td>
+		<td>1255</td>
+	</tr>
+	<tr>
+		<td>Eaux Endormies</td>
+		<td>146</td>
+		<td>137</td>
+		<td>155</td>
+		<td>730</td>
+	</tr>
+	<tr>
+		<td>Les Archipels</td>
+		<td>169</td>
+		<td>156</td>
+		<td>181</td>
+		<td>845</td>
+	</tr>
+	<tr>
+		<td>Mont Qinkai</td>
+		<td>162</td>
+		<td>133</td>
+		<td>190</td>
+		<td>810</td>
+	</tr>
+	<tr>
+		<td>Fonds marins de Boreas</td>
+		<td>195</td>
+		<td>160</td>
+		<td>229</td>
+		<td>975</td>
+	</tr>
+	<tr>
+		<td>Ressac silencieux</td>
+		<td>264</td>
+		<td>244</td>
+		<td>283</td>
+		<td>1320</td>
+	</tr>
+</table>
+<br/>
+<br/>
+
+<!-- Nombre de moobs dans Elona (Nightfall)  -->
+
+<table align="center">
+	<caption>Elona</caption>
+	
+	<tr>
+		<th>Zone d'exploration</th>
+		<th>Nombre moyen</th>
+		<th>Nombre mimimum</th>
+		<th>Nombre maximun</th>
+		<th>Po et xp moyen</th>
+	</tr>
+
+	<tr>
+		<td colspan="5"><span id="Istan"><strong>Istan</strong></span></td>
+	</tr>
+	<tr>
+		<td>Archipel de Mehtani</td>
+		<td>243</td>
+		<td>210</td>
+		<td>276</td>
+		<td>1215</td>
+	</tr>
+	<tr>
+		<td>Etendues de Zehlon</td>
+		<td>242</td>
+		<td>222</td>
+		<td>262</td>
+		<td>1210</td>
+	</tr>
+	<tr>
+		<td>Fahranur, première cité</td>
+		<td>208</td>
+		<td>195</td>
+		<td>220</td>
+		<td>1040</td>
+	</tr>
+	<tr>
+		<td>Falaises de Dohjok</td>
+		<td>157</td>
+		<td>113</td>
+		<td>201</td>
+		<td>785</td>
+	</tr>
+	<tr>
+		<td>Iles d'Issnur</td>
+		<td>347</td>
+		<td>339</td>
+		<td>354</td>
+		<td>1735</td>
+	</tr>
+	<tr>
+		<td>Marais de Lahtenda</td>
+		<td>401</td>
+		<td>372</td>
+		<td>430</td>
+		<td>2005</td>
+	</tr>
+	<tr>
+		<td>Plaines de  Jarin</td>
+		<td>204</td>
+		<td>189</td>
+		<td>218</td>
+		<td>1020</td>
+	</tr>
+
+	<tr>
+		<td colspan="5"><span id="Kourna"><strong>Kourna</strong></span></td>
+	</tr>
+	<tr>
+		<td>Cavernes Bahdok</td>
+		<td>287</td>
+		<td>273</td>
+		<td>301</td>
+		<td>1435</td>
+	</tr>
+	<tr>
+		<td>Côte Barbare</td>
+		<td>187</td>
+		<td>173</td>
+		<td>201</td>
+		<td>935</td>
+	</tr>
+	<tr>
+		<td>Côte de Marga</td>
+		<td>292</td>
+		<td>254</td>
+		<td>330</td>
+		<td>1460</td>
+	</tr>
+	<tr>
+		<td>Domaine de Dejarin</td>
+		<td>249</td>
+		<td>230</td>
+		<td>268</td>
+		<td>1245</td>
+	</tr>
+	<tr>
+		<td>Gandara, la Forteresse de la lune</td>
+		<td>94</td>
+		<td>64</td>
+		<td>123</td>
+		<td>470</td>
+	</tr>
+	<tr>
+		<td>Marais du soleil</td>
+		<td>299</td>
+		<td>282</td>
+		<td>315</td>
+		<td>1495</td>
+	</tr>
+	<tr>
+		<td>Plaines inondables de Mahnkelon</td>
+		<td>229</td>
+		<td>206</td>
+		<td>252</td>
+		<td>1145</td>
+	</tr>
+	<tr>
+		<td>Procession deTurai</td>
+		<td>202</td>
+		<td>163</td>
+		<td>240</td>
+		<td>1010</td>
+	</tr>
+	<tr>
+		<td>Promontoire de Jahai</td>
+		<td>288</td>
+		<td>265</td>
+		<td>310</td>
+		<td>1440</td>
+	</tr>
+	<tr>
+		<td>Quartier d'Arkjok</td>
+		<td>324</td>
+		<td>286</td>
+		<td>362</td>
+		<td>1620</td>
+	</tr>
+
+	<tr>
+		<td colspan="5"><span id="Vabbi"><strong>Vabbi</strong></span></td>
+	</tr>
+	<tr>
+		<td>Canyons de Yatendi</td>
+		<td>196</td>
+		<td>193</td>
+		<td>198</td>
+		<td>980</td>
+	</tr>
+	<tr>
+		<td>Citée cachée d'Ahdashim</td>
+		<td>222</td>
+		<td>207</td>
+		<td>236</td>
+		<td>1110</td>
+	</tr>
+	<tr>
+		<td>Jardin de Seborhin</td>
+		<td>265</td>
+		<td>225</td>
+		<td>304</td>
+		<td>1325</td>
+	</tr>
+	<tr>
+		<td>Le Miroir de Lyss</td>
+		<td>201</td>
+		<td>159</td>
+		<td>243</td>
+		<td>1005</td>
+	</tr>
+	<tr>
+		<td>Makuun Resplendissant</td>
+		<td>167</td>
+		<td>157</td>
+		<td>176</td>
+		<td>835</td>
+	</tr>
+	<tr>
+		<td>Mines de Vehjin</td>
+		<td>166</td>
+		<td>140</td>
+		<td>192</td>
+		<td>830</td>
+	</tr>
+	<tr>
+		<td>Montagnes de la tribune</td>
+		<td>288</td>
+		<td>244</td>
+		<td>331</td>
+		<td>1440</td>
+	</tr>
+	<tr>
+		<td>Terres sauvages de Bahdza</td>
+		<td>168</td>
+		<td>159</td>
+		<td>176</td>
+		<td>840</td>
+	</tr>
+	<tr>
+		<td>Territoire de Chokhin</td>
+		<td>246</td>
+		<td>244</td>
+		<td>248</td>
+		<td>1230</td>
+	</tr>
+	<tr>
+		<td>Vallée de Vehtendi</td>
+		<td>253</td>
+		<td>248</td>
+		<td>257</td>
+		<td>1265</td>
+	</tr>
+
+	<tr>
+			<td colspan="5"><span id="Desolation"><strong>La Désolation</strong></span></td>
+	</tr>
+	<tr>
+		<td>Coeur déchiré</td>
+		<td>241</td>
+		<td>238</td>
+		<td>243</td>
+		<td>1205</td>
+	</tr>
+	<tr>
+		<td>Domaine de Joko</td>
+		<td>426</td>
+		<td>414</td>
+		<td>438</td>
+		<td>2130</td>
+	</tr>
+	<tr>
+		<td>Désert Sulfureux</td>
+		<td>307</td>
+		<td>241</td>
+		<td>372</td>
+		<td>1535</td>
+	</tr>
+	<tr>
+		<td>Les Ravins dévastés</td>
+		<td>307</td>
+		<td>285</td>
+		<td>329</td>
+		<td>1535</td>
+	</tr>
+	<tr>
+		<td>Plateau d'Alkali</td>
+		<td>300</td>
+		<td>281</td>
+		<td>319</td>
+		<td>1500</td>
+	</tr>
+	<tr>
+		<td>Plateau de Cristal</td>
+		<td>197</td>
+		<td>193</td>
+		<td>201</td>
+		<td>985</td>
+	</tr>
+	<tr>
+		<td>Saillies empoisonnées</td>
+		<td>211</td>
+		<td>176</td>
+		<td>246</td>
+		<td>1055</td>
+	</tr>
+	
+</table>
+
+<br/>
+<br/>
+
+<!-- Nombre de moobs en Tyrie (Eye of the Nord)  -->
+
+<table align="center">
+	<caption>Gwen</caption>
+	
+	<tr>
+		<th>Zone d'exploration</th>
+		<th>Nombre moyen</th>
+		<th>Nombre mimimum</th>
+		<th>Nombre maximun</th>
+		<th>Po et xp moyen</th>
+	</tr>
+
+	<tr>
+		<td colspan="5"><span id="Profondeur"><strong>Profondeur des Cimesfroides</strong></span></td>
+	</tr>
+	<tr>
+		<td>Domaines de Norrhart</td>
+		<td>251</td>
+		<td>248</td>
+		<td>254</td>
+		<td>1255</td>
+	</tr>
+	<tr>
+		<td>Frontière de Bjora</td>
+		<td>234</td>
+		<td>229</td>
+		<td>239</td>
+		<td>1170</td>
+	</tr>
+	<tr>
+		<td>Gouffre de Givre</td>
+		<td>150</td>
+		<td>141</td>
+		<td>158</td>
+		<td>750</td>
+	</tr>
+	<tr>
+		<td>Lac Drakkar</td>
+		<td>370</td>
+		<td>330</td>
+		<td>410</td>
+		<td>1850</td>
+	</tr>
+	<tr>
+		<td>Lande de Varajar</td>
+		<td>501</td>
+		<td>481</td>
+		<td>520</td>
+		<td>2505</td>
+	</tr>
+	<tr>
+		<td>Moraine de Jaga</td>
+		<td>269</td>
+		<td>263</td>
+		<td>274</td>
+		<td>1345</td>
+	</tr>
+	
+	<tr>
+		<td colspan="5"><span id="char"><strong>Charr</strong></span></td>
+	</tr>
+	<tr>
+		<td>Collines de la Guerre de Grothmar</td>
+		<td>236</td>
+		<td>236</td>
+		<td>236</td>
+		<td>1180</td>
+	</tr>
+	<tr>
+		<td>Plateaux de Dalada</td>
+		<td>286</td>
+		<td>283</td>
+		<td>288</td>
+		<td>1430</td>
+	</tr>
+	<tr>
+		<td>Vallée de Sacnoth</td>
+		<td>414</td>
+		<td>390</td>
+		<td>438</td>
+		<td>2070</td>
+	</tr>
+
+	
+	<tr>
+		<td colspan="5"><span id="cotes"><strong>Côtes ternies</strong></span></td>
+	</tr>
+	
+	<tr>
+		<td>Baie d'Arbor</td>
+		<td>443</td>
+		<td>375</td>
+		<td>511</td>
+		<td>2215</td>
+	</tr>
+	<tr>
+		<td>Cascades verdoyantes</td>
+		<td>291</td>
+		<td>290</td>
+		<td>292</td>
+		<td>1455</td>
+	</tr>
+	<tr>
+		<td>Lacis d'Alcazia</td>
+		<td>227</td>
+		<td>220</td>
+		<td>234</td>
+		<td>1135</td>
+	</tr>
+	<tr>
+		<td>Marais de la Lumilule</td>
+		<td>322</td>
+		<td>322</td>
+		<td>322</td>
+		<td>1610</td>
+	</tr>
+	<tr>
+		<td>Pierres des Mages</td>
+		<td>394</td>
+		<td>379</td>
+		<td>409</td>
+		<td>1970</td>
+	</tr>
+	<tr>
+		<td>Terre Déchirée</td>
+		<td>277</td>
+		<td>260</td>
+		<td>293</td>
+		<td>1385</td>
+	</tr>
+	
+	
+</table>
+
+
+<?php
+	include '../footer.php';
+?>
